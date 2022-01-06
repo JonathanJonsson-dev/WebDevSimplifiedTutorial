c5d25e640396d46830e98b1ef79f5fc894cf0cdb
Jonathan Jonsson <jonsson.jonathan@hotmail.com>
2022-01-06 10:21:19 +0100

    Added .env file

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+// connects to database
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (error)=>{console.error(error);})
+db.once('open', ()=>{console.log('Connected to database');})
+
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000) 
